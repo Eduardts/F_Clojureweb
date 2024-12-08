4962cd86169366b4caac42f1fceb9abdc4556737
edi <hometigereduard@gmail.com>
2024-12-08 11:16:42 +0200

    com

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-website
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_website/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-website.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-website.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
